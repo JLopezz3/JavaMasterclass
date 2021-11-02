028a2425bd3e8ea46ba644b07dc1684a84ae2340
JLopezz3 <lopezjerel@gmail.com>
2021-11-02 18:40:29 -0500

    BarkingDog

---
+++ BarkingDog/src/com/example/java/BarkingDog.java
@@ -0,0 +1,16 @@
+package com.example.java;
+
+public class BarkingDog {
+
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+
+        if (!barking || (hourOfDay < 0 || hourOfDay > 23)) {
+            return false;
+        }
+        if (hourOfDay < 8 || hourOfDay > 22 && barking) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ BarkingDog/src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean response = BarkingDog.shouldWakeUp(true, -1);
+        System.out.println(response);
+
+
+    }
+}
