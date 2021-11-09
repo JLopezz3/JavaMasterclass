bd7a74592faf21911a57d82b1dd0631c53dd728d
JLopezz3 <lopezjerel@gmail.com>
2021-11-09 10:16:04 -0600

    EqualityPrinter

---
+++ EqualityPrinter/src/com/example/java/EqualityPrinter.java
@@ -0,0 +1,23 @@
+package com.example.java;
+
+public class EqualityPrinter {
+
+    public static void printEqual (int a, int b, int c){
+
+        if((a < 0) || (b < 0) || (c < 0)){
+            System.out.println("Invalid Value");
+        } else if((a == b) && (a == c)){
+            System.out.println("All numbers are equal");
+        } else if ((a == b) && (a != c)){
+            System.out.println("Neither all are equal or different");
+        } else if ((a == c) && (a != b)){
+            System.out.println("Neither all are equal or different");
+        } else if ((b == c) && (a != b)){
+            System.out.println("Neither all are equal or different");
+        } else {
+            System.out.println("All numbers are different");
+        }
+
+    }
+
+}
---
+++ EqualityPrinter/src/com/example/java/Main.java
@@ -0,0 +1,13 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EqualityPrinter.printEqual(1,1,1);
+        EqualityPrinter.printEqual(1,1,2);
+        EqualityPrinter.printEqual(-1,-1,-1);
+        EqualityPrinter.printEqual(1,2,3);
+
+    }
+}
