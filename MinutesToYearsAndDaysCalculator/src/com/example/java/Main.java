10a80f5471ae4ca2533b9627b78fb1fb12589877
JLopezz3 <lopezjerel@gmail.com>
2021-11-09 10:05:11 -0600

    MinutesToYearsAndDays

---
+++ MinutesToYearsAndDaysCalculator/src/com/example/java/Main.java
@@ -0,0 +1,12 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MinutesToYearsAndDays.printYearsAndDays(525600);
+        MinutesToYearsAndDays.printYearsAndDays(1051200);
+        MinutesToYearsAndDays.printYearsAndDays(561600);
+
+    }
+}
