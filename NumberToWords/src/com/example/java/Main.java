e3c7c2074260efc6eec6b93fbc27c8f9ec6f51fe
JLopezz3 <lopezjerel@gmail.com>
2021-11-16 18:21:18 -0600

    NumberToWords

---
+++ NumberToWords/src/com/example/java/Main.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NumberToWord.printNumberInWord(1);
+        NumberToWord.printNumberInWord(-1);
+
+    }
+}
