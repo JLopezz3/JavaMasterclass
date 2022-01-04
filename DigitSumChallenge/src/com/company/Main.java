dd2684659933d86f0225a36ac506885707585b11
JLopezz3 <58873590+JLopezz3@users.noreply.github.com>
2022-01-03 20:22:22 -0600

    DigitSum

---
+++ DigitSumChallenge/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The sum of digits in number 125 is " + sumDigit(125));
+        System.out.println("The sum of digits in number -125 is " + sumDigit(-125));
+        System.out.println("The sum of digits in number 4 is " + sumDigit(4));
+        System.out.println("The sum of digits in number 32123 is " + sumDigit(32123));
+    }
+
+    private static int sumDigit(int number) {
+
+        if(number < 10){
+            return -1;
+        }
+
+        int sum = 0;
+
+        // 125 -> 125 / 10 = 12 -> 12 * 10 = 120 -> 125 - 120 = 5
+        while(number > 0){
+            // extract the least-significant digit
+            int digit = number % 10;
+            sum += digit;
+
+            // drop the least-significant digit
+            number /= 10;  // same as number = number 10;
+        }
+
+        return sum;
+    }
+
+}
